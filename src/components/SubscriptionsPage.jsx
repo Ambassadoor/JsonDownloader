7a1ca357e0f35257f54a2c5791ff5eef7d845001
Pittman <ctpittman@lipscomb.edu>
2024-08-13 12:04:19 -0500

    added course selector functionality to subscriptions

--- src/components/SubscriptionsPage.jsx
+++ src/components/SubscriptionsPage.jsx
@@ -1,35 +1,54 @@
-// src/components/SubscriptionsPage.jsx
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AppStateContext } from "../AppStateContext";
 import CourseViewer from "./CourseViewer";
 import createEventFromCourse from "./CalendarEntryGenerator";
 
 const SubscriptionsPage = () => {
   const { subscribedCourses } = useContext(AppStateContext);
-  const eventObjects = subscribedCourses.map(createEventFromCourse);
+  const [currentCourseIndex, setCurrentCourseIndex] = useState(0);
+  const [currentCourse, setCurrentCourse] = useState(null);
+  const [eventObjects, setEventObjects] = useState([]);
+
+  useEffect(() => {
+    const course = subscribedCourses[currentCourseIndex];
+    setCurrentCourse(course);
+    setEventObjects([createEventFromCourse(course)]);
+  }, [currentCourseIndex, subscribedCourses]);
+
+  const handleNextCourse = () => {
+    setCurrentCourseIndex((prevIndex) =>
+      prevIndex < subscribedCourses.length - 1 ? prevIndex + 1 : prevIndex,
+    );
+  };
+
+  const handlePrevCourse = () => {
+    setCurrentCourseIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : prevIndex,
+    );
+  };
 
   return (
     <div>
       <h1>Subscribed Courses</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Course Code</th>
-            <th>Section Code</th>
-            <th>Course Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {subscribedCourses.map((course, index) => (
-            <tr key={index}>
-              <td>{course["Course Code"]}</td>
-              <td>{course["Section Code"]}</td>
-              <td>{course["Course Name"]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <CourseViewer eventObjects={eventObjects} />
+      <div>
+        <button onClick={handlePrevCourse} disabled={currentCourseIndex === 0}>
+          Previous
+        </button>
+        {currentCourse && (
+          <span>
+            {`Viewing course ${currentCourseIndex + 1} of ${
+              subscribedCourses.length
+            }: ${currentCourse["Course Name"]}`}
+          </span>
+        )}
+        <button
+          onClick={handleNextCourse}
+          disabled={currentCourseIndex === subscribedCourses.length - 1}
+        >
+          Next
+        </button>
+      </div>
+      {eventObjects.length > 0 && <CourseViewer eventObjects={eventObjects} />}
     </div>
   );
 };
