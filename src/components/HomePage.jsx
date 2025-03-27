edaa5ab691f6c7843bd6dc7657ae9ac9901dc1f0
Caleb  Pittman <96952211+Ambassadoor@users.noreply.github.com>
2025-03-27 13:29:27 -0500

    Reorganized elements on home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
+import { Box, Grid } from "@mui/material";
 import CourseTransferList from "./CourseTransferList";
 import SemesterSelectorUI from "./SemesterSelectorUI";
 import { AppStateContext } from "../AppStateContext";
@@ -33,8 +34,10 @@ const HomePage = () => {
 
   return (
     <div className="app-container">
-      <CourseTransferList />
-      <SemesterSelectorUI onSelectSemester={handleSemesterSelection} />
+      <Box>
+        <SemesterSelectorUI onSelectSemester={handleSemesterSelection} />
+        <CourseTransferList />
+      </Box>
     </div>
   );
 };
